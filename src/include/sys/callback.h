296cc8eacd9ed6395b3111685faa1e573b0e85a4
primis <primis@primiscomputers.com>
2014-06-06 12:52:55 -0400

    Added more Primatives for callbacks

--- src/include/sys/callback.h
+++ src/include/sys/callback.h
@@ -6,10 +6,44 @@
 #ifndef _SYS_CALLBACK_H
 #define _SYS_CALLBACK_H
 
+// Void Return Primatives
 typedef void (*void_callback_t)(void);
 typedef void (*void_callback_char_t)(char);
 typedef void (*void_callback_short_t)(short);
 typedef void (*void_callback_int_t)(int);
-typedef void (*void_callback_unsigned_int_t)(unsigned int);
+/// Unsigned Versions
+typedef void (*void_callback_char_t)(unsigned char);
+typedef void (*void_callback_short_t)(unsigned short);
+typedef void (*void_callback_int_t)(unsigned int);
+
+// Char Return Primatives
+typedef char (*void_callback_t)(void);
+typedef char (*void_callback_char_t)(char);
+typedef char (*void_callback_short_t)(short);
+typedef char (*void_callback_int_t)(int);
+/// Unsigned Versions
+typedef char (*void_callback_char_t)(unsigned char);
+typedef char (*void_callback_short_t)(unsigned short);
+typedef char (*void_callback_int_t)(unsigned int);
+
+// Int Return Primatives
+typedef int (*void_callback_t)(void);
+typedef int (*void_callback_char_t)(char);
+typedef int (*void_callback_short_t)(short);
+typedef int (*void_callback_int_t)(int);
+/// Unsigned Versions
+typedef int (*void_callback_char_t)(unsigned char);
+typedef int (*void_callback_short_t)(unsigned short);
+typedef int (*void_callback_int_t)(unsigned int);
+
+// Short Return Primatives
+typedef short (*void_callback_t)(void);
+typedef short (*void_callback_char_t)(char);
+typedef short (*void_callback_short_t)(short);
+typedef short (*void_callback_int_t)(int);
+/// Unsigned Versions
+typedef short (*void_callback_char_t)(unsigned char);
+typedef short (*void_callback_short_t)(unsigned short);
+typedef short (*void_callback_int_t)(unsigned int);
 
 #endif
