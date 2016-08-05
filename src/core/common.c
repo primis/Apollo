7ca201ab930c8063539edfc06c9068539804306a
primis <nick@primis.org>
2016-08-05 10:11:49 -0400

    New rewrite, Clearing master branch Old Master is now "backburner"
    Signed-off-by: primis <nick@primis.org>

--- src/core/common.c
+++
@@ -1,9 +0,0 @@
-/*
- * (c) 2016 Apollo Developers
- * common.c - Stuff that doesn't quite fit anywhere else
- */
-
-#include <sys/init.h>
-
-// Makefile includes Git build number as a macro string.
-const char* gitRevision = GITREV;
