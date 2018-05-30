f629ac00e7108112f7b34490a8bfd85821420f08
primis <allie@sargente.org>
2018-05-30 18:12:55 -0400

    Testing VGA framebuffer accuracy

--- src/core/main.c
+++ src/core/main.c
@@ -13,8 +13,8 @@ int main(multiboot_info_t* mbt)
     console_write("\nArch Enabled. Initializing Base System...\n");
     console_write("Multiboot header located at ");
     console_write_hex((unsigned int)&mbt);
-    console_write(". flags are defined as: ");
-    console_write_hex(mbt->flags);
+    console_write(". VGA Framebuffer is defined at: ");
+    console_write_hex(mbt->framebuffer_addr & 0xFFFFFFFF);
     console_write(".\n");
     asm volatile ("int 0x03");
     return 0xDEADC0DE;
