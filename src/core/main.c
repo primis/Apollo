08b431475010d175d5b9b0bb62d264d96759bdae
primis <allie@sargente.org>
2018-05-30 17:57:48 -0400

    Test 3

--- src/core/main.c
+++ src/core/main.c
@@ -7,19 +7,12 @@
 #include <arch/x86/idt.h>
 #include <sys/multiboot.h>
 
-int main(multiboot_info_t* mbt, uint32_t magic)
+int main(multiboot_info_t* mbt)
 {
-    // Memory map for later use.
-    multiboot_memory_map_t* mmap = (multiboot_memory_map_t*)mbt->mmap_addr;
-
     archInit();
-    console_write_hex(magic);
     console_write("\nArch Enabled. Initializing Base System...\n");
-    while((uint32_t)mmap < mbt->mmap_addr + mbt->mmap_length) {
-        mmap = (multiboot_memory_map_t*) ((uint32_t)mmap + mmap->size + sizeof(mmap->size));
-    }
-    console_write("Found Memory, KBytes: ");
-    console_write_hex(mmap->addr);
+    console_write("Multiboot header located at ");
+    console_write_hex((unsigned int)&mbt);
     asm volatile ("int 0x03");
     return 0xDEADC0DE;
 }
